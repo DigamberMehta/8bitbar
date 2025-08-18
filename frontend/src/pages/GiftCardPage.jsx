fb750c4c874977a9cd3b434c769a799837352e4a
digamber <digambermehta2603@gmail.com>
2025-08-19 03:51:33 +0530

    done

--- frontend/src/pages/GiftCardPage.jsx
+++ frontend/src/pages/GiftCardPage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Footer from "../components/Footer";
 import GiftCardPurchase from "../components/GiftCardPurchase";
 
 const GiftCardPage = () => {
@@ -8,7 +7,6 @@ const GiftCardPage = () => {
       <main>
         <GiftCardPurchase />
       </main>
-      <Footer />
     </>
   );
 };
